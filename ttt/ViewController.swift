da96bfe15ccb83c969cc5c4b633a800425d77a94
MJ Ling <mjling@mjdemacbook-pro.local>
2018-01-12 11:46:44 +0800

    Initial Commit

---
+++ ttt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by MJ Ling on 2018/1/12.
+//  Copyright © 2018年 MJ Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
