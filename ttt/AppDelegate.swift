2ba4b5a9d202ef8638cf6d3b8b962540b015109a
Ling Minjun <lingminjun@lingminjun-MacBook-Pro.local>
2018-03-07 16:58:40 +0800

    引入brief验签

--- ttt/AppDelegate.swift
+++ ttt/AppDelegate.swift
@@ -36,7 +36,126 @@ public class AppDelegate: UIResponder, UIApplicationDelegate,Authorize {
         Navigator.shared.open("https://m.mymm.com")
         Navigator.shared.setAuthorize(auth: self)
         window?.makeKeyAndVisible()
+        
+//        //        (n,e)公钥 (3233, 17)
+//        //        (n,d)私钥 (3233, 2753)
+//        let n = BigInt8(truncatingIfNeeded:3233)
+//        let e = BigInt8(truncatingIfNeeded:17)
+//        let d = BigInt8(truncatingIfNeeded:2753)
+//
+//        let m = 65
+//        let mc = BigInt8(truncatingIfNeeded:2790)
+//
+//        let c = BigInt8.pow(base:BigInt8(truncatingIfNeeded:m),exponent:e) % n  // 加密 m^e ≡ c (mod n)
+//
+//        if c == mc {
+//            print("验证通过")
+//        }
+//
+//        let a = BigInt8(truncatingIfNeeded:2)
+//        let b = BigInt8(truncatingIfNeeded:3)
+//
+//        let x = BigInt8.pow(base: a, exponent: b)// a ^ b
+//        if x == BigInt8(truncatingIfNeeded:8) {
+//            print("验证通过")
+//        }
+        
+//        if let bi = BInt.init("10000000", radix: 2) {
+//            let b = bi.bytes
+//            print("\(b)")
+//
+//            let xb = BInt.init(bytes: b)
+//            print("\(xb)")
+//
+//            if bi == xb {
+//                print("==========")
+//            }
+//        }
+//
+//        let bi = BInt.init(integerLiteral: -5)
+//            let b = bi.bytes
+//            print("\(b)")
+//
+//            let xb = BInt.init(bytes: b)
+//            print("\(xb)")
+//
+//            if bi == xb {
+//                print("==========")
+//            }
+//        }
+//        rsa()
+        
+        
         return true
+    }
+    
+    public static let PUB_KEY = "ZDY2YjcwZDY0ZjE3OGY3KzEwMDAx";
+    
+    //请妥善保存私钥，编译时请删除
+    private static let PRI_KEY = "ZDY2YjcwZDY0ZjE3OGY3KzMyNDUyYzQ0OGFmNDM2MQ==";
+    
+    private func rsa() {
+        let pub_key = "ZDY2YjcwZDY0ZjE3OGY3KzEwMDAx";
+        let pri_key = "ZDY2YjcwZDY0ZjE3OGY3KzMyNDUyYzQ0OGFmNDM2MQ==";
+        
+        
+        //            genRSA(800000000,1000000000);
+        if true {
+            let msg = "stackoverflow.com";
+            let sign = "BmfRYZPsJKE=";
+            print(sign);
+            if let d = "stackoverflow.com".data(using: String.Encoding.utf8) {
+                if (BriefRSA.verify(key:AppDelegate.PUB_KEY,sign:"BmfRYZPsJKE=",data:d)) {
+                    print("verify true");
+                }
+            }
+        }
+        
+        if true {
+            let msg = "234455";
+            if let msgD = msg.data(using: String.Encoding.utf8) {
+                let sign = BriefRSA.sign(key:pri_key,data:msgD);
+                print(sign);
+                if (BriefRSA.verify(key:pub_key,sign:sign,data:msgD)) {
+                    print("verify true");
+                }
+            }
+        }
+        
+        if true {
+            let msg = "www.fengqu.com";
+            if let msgD = msg.data(using: String.Encoding.utf8) {
+                let sign = BriefRSA.sign(key:pri_key,data:msgD);
+                print(sign);
+                if (BriefRSA.verify(key:pub_key,sign:sign,data:msgD)) {
+                    print("verify true");
+                }
+            }
+        }
+        
+        if true {
+            let msg = "肖信波 杨世亮";
+            if let msgD = msg.data(using: String.Encoding.utf8) {
+                let sign = BriefRSA.sign(key:pri_key,data:msgD);
+                print(sign);
+                if (BriefRSA.verify(key:pub_key,sign:sign,data:msgD)) {
+                    print("verify true");
+                }
+            }
+        }
+        
+        if true {
+            let msg = "打算大家送大礼斯柯达dhjkasakda哈佛爱的大声道啊";
+            if let msgD = msg.data(using: String.Encoding.utf8) {
+                let sign = BriefRSA.sign(key:pri_key,data:msgD);
+                print(sign);
+                if (BriefRSA.verify(key:pub_key,sign:sign,data:msgD)) {
+                    print("verify true");
+                }
+            }
+        }
+        
+        
     }
 
     public func applicationWillResignActive(_ application: UIApplication) {
