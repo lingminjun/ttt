cd5e9e01e527e917b8ccc6f1416cb8723935e313
Ling Minjun <lingminjun@lingminjun-MacBook-Pro.local>
2018-07-13 17:29:54 +0800

    修改magic实现

--- ttt/core/demo/SCollectionViewController.swift
+++ ttt/core/demo/SCollectionViewController.swift
@@ -97,7 +97,7 @@ class SCollectionViewController : MMUIController,UICollectionViewDelegate,UIColl
             _config.rowHeight = 0
             _config.columnCount = 2
             _config.columnSpace = 10
-            _config.magicHorizontalEdge = 10
+            _config.supportMagicHorizontalEdge = true
             _config.floating = false
         } else if title == "瀑布流飘浮" {
             _config.rowHeight = 0
@@ -157,8 +157,8 @@ class SCollectionViewController : MMUIController,UICollectionViewDelegate,UIColl
         return CGFloat(_datas[indexPath.section][indexPath.row])
     }
     
-    func collectionView(_ collectionView: UICollectionView, supportMagicEdgeForCellAt indexPath: IndexPath) -> Bool {
-        return true
+    func collectionView(_ collectionView: UICollectionView, magicHorizontalEdgeForCellAt indexPath: IndexPath) -> CGFloat {
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
